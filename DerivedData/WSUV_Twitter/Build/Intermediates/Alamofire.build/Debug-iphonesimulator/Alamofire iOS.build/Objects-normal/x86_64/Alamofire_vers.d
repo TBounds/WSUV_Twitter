c0a27687105bc171269feb1c8346ebbbd2836697
Tyler James Bounds <t.bounds@ecs123-3.labs.encs>
2017-03-29 10:57:30 -0700

    Adding alamofire to the project.

---
+++ DerivedData/WSUV_Twitter/Build/Intermediates/Alamofire.build/Debug-iphonesimulator/Alamofire iOS.build/Objects-normal/x86_64/Alamofire_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /home-local/t.bounds/WSUV_Twitter/DerivedData/WSUV_Twitter/Build/Intermediates/Alamofire.build/Debug-iphonesimulator/Alamofire\ iOS.build/DerivedSources/Alamofire_vers.c
